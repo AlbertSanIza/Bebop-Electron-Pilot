9420783da9d1225b44915d16d3589d2d2658e465
Albert Sanchez <albert.san.iza@gmail.com>
2016-03-31 21:00:02 -0700

    Updated to work with macOS

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 var app = require('app');
 var ipc = require('ipc');
-var cylon = require('cylon');
 var bebop = require('node-bebop');
 var browserWindow = require('browser-window');
 
@@ -29,45 +28,79 @@ app.on('ready', function() {
     win = null;
   });
 
-  ipc.on('electron-message', function(event, arg) {
-    switch (arg) {
-      case "connect":
-      var drone = bebop.createClient();
-      event.sender.send('electron-message-reply', 'connect');
-      break;
-      case "takeOff":
-      break;
-      case "land":
-      break;
-      case "stop":
-      break;
-      case "up":
-      break;
-      case "downs":
-      break;
-      case "left":
-      break;
-      case "right":
-      break;
-      case "stop":
-      break;
-      case "stop":
-      break;
-      case "stop":
-      break;
-      case "stop":
-      break;
-      case "stop":
-      break;
-      case "stop":
-      break;
-      case "stop":
-      break;
-      case "stop":
-      break;
-      case "stop":
-      break;
-    }
+  var drone = bebop.createClient();
+
+  drone.connect(function() {
+    console.log("\n\n ** Connected to Drone ** \n\n");
+
+    ipc.on('electron-message', function(event, arg) {
+      switch (arg) {
+        case "connect":
+        event.sender.send('electron-message-reply', 'connect');
+        break;
+        case "takeOff":
+        console.log("\ntakeOff\n");
+        drone.takeOff();
+        break;
+        case "land":
+        console.log("\nland\n");
+        drone.land();
+        break;
+        case "stop":
+        console.log("\nstop\n");
+        drone.stop();
+        break;
+        case "up":
+        console.log("\nup\n");
+        drone.up(5);
+        break;
+        case "down":
+        console.log("\ndown\n");
+        drone.down(5);
+        break;
+        case "left":
+        console.log("\nleft\n");
+        drone.left(5);
+        break;
+        case "right":
+        console.log("\nright\n");
+        drone.right(5);
+        break;
+        case "forward":
+        console.log("\nforward\n");
+        drone.forward(5);
+        break;
+        case "backward":
+        console.log("\nbackward\n");
+        drone.backward(5);
+        break;
+        case "clockwise":
+        console.log("\nclockwise\n");
+        drone.clockwise(5);
+        break;
+        case "counterClockwise":
+        console.log("\ncounterClockwise\n");
+        drone.counterClockwise(5);
+        break;
+        case "frontFlip":
+        console.log("\nfrontFlip\n");
+        break;
+        case "backFlip":
+        console.log("\nbackFlip\n");
+        break;
+        case "rightFlip":
+        console.log("\nrightFlip\n");
+        break;
+        case "leftFlip":
+        console.log("\nleftFlip\n");
+        break;
+        case "emergency":
+        console.log("\nemergency\n");
+        drone.emergency();
+        break;
+      }
+    });
+
   });
 
 });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,4 +1,4 @@
-const ipcRenderer = require('electron').ipcRenderer;
+var ipc = require('ipc');
 //------------------------------------------------------------------------------
 angular.module('starter.controllers', [])
 //------------------------------------------------------------------------------
@@ -10,7 +10,7 @@ angular.module('starter.controllers', [])
 
   $scope.openModal = function() {
     console.log("Connect");
-    ipcRenderer.send('electron-message', 'connect');
+    ipc.send('electron-message', 'connect');
   };
 
   $scope.closeModal = function() {
@@ -27,7 +27,7 @@ angular.module('starter.controllers', [])
     console.log("Take Off");
     if($scope.bebop.takeOff == true) {
       $scope.bebop.takeOff = !$scope.bebop.takeOff;
-      ipcRenderer.send('electron-message', 'takeOff');
+      ipc.send('electron-message', 'takeOff');
     }
   };
   $scope.land = function() {
@@ -35,73 +35,88 @@ angular.module('starter.controllers', [])
     console.log("Land");
     if($scope.bebop.takeOff == false) {
       $scope.bebop.takeOff = !$scope.bebop.takeOff;
+      ipc.send('electron-message', 'land');
     }
   };
   $scope.stop = function() {
     //Sets all drone movement commands to 0, making it effectively hover in place.
     console.log("Stop");
+    ipc.send('electron-message', 'stop');
   };
   $scope.up = function() {
     //Tells the drone to start gaining altitude
     console.log("Up");
+    ipc.send('electron-message', 'up');
   };
   $scope.down = function() {
     //Tells the drone to start losing altitude
     console.log("Down");
+    ipc.send('electron-message', 'down');
   };
   $scope.left = function() {
     //Tells the drone to bank left - controlling the roll using the camera as a reference point
     console.log("Left");
+    ipc.send('electron-message', 'left');
   };
   $scope.right = function() {
     //Tells the drone to bank right - controlling the roll using the camera as a reference point
     console.log("Right");
+    ipc.send('electron-message', 'right');
   };
   $scope.forward = function() {
     //Tells the drone to bank forward - controlling the pitch using the camera as a reference points
     console.log("Forward");
+    ipc.send('electron-message', 'forward');
   };
   $scope.backward = function() {
     //Tells the drone to bank backwards - controlling the pitch using the camera as a reference point
     console.log("Backward");
+    ipc.send('electron-message', 'backward');
   };
   $scope.clockwise = function() {
     //Tells the drone to begin spinning clockwise
     console.log("Clockwise");
+    ipc.send('electron-message', 'clockwise');
   };
   $scope.counterClockwise = function() {
     //Tells the drone to begin spinning counterclockwise
     console.log("Counter Clockwise");
+    ipc.send('electron-message', 'counterClockwise');
   };
   $scope.frontFlip = function() {
     //Tells the drone to frontFlip
     console.log("Front Flip");
+    ipc.send('electron-message', 'frontFlip');
   };
   $scope.backFlip = function() {
     //Tells the drone to backFlip
     console.log("Back Flip");
+    ipc.send('electron-message', 'backFlip');
   };
   $scope.rightFlip = function() {
     //Tells drone to rightflip
     console.log("Right Flip");
+    ipc.send('electron-message', 'rightFlip');
   };
   $scope.leftFlip = function() {
     //Tells drone to leftflip
     console.log("Left Flip");
+    ipc.send('electron-message', 'leftFlip');
   };
   $scope.emergency = function() {
     //Tell the drone to drop like a stone
     console.log("Emergency");
+    ipc.send('electron-message', 'emergency');
     $scope.bebop.takeOff = true;
   };
 
-  ipcRenderer.on('electron-message-reply', function(event, arg) {
+  ipc.on('electron-message-reply', function(arg) {
+    console.log(arg);
     switch (arg) {
       case "connect":
       $scope.modal.show();
       break;
     }
-
   });
 
 })
